8c12831ede05165101235ee1758403199beba3e7
Harsh Sharma <112812978+harsharma30@users.noreply.github.com>
2024-07-15 16:40:45 +0530

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,29 @@
+# FOR LOOP
+
+n = int(input("EnteR a number: "))
+'''
+for i in range(11):
+    if i==0:
+        continue
+    else:
+        print(n, "X", i, "=", n*i)
+'''       
+
+# with ranges
+'''
+for i in range(1,21):            # with start and ending ranges
+    print(n, "X", i, "=", n*i)
+'''
+
+#with intervals
+'''
+for i in range(1, 11, 2):       #start,end,interval
+    print(n, "X", i, "=", n*i)
+'''
+
+#NESTED FOR LOOP
+'''
+for i in range(1,11):
+    for j in range(1,11):
+        print(i,j)
+'''
